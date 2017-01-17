8e56be5afb87f4101d7a7d93206ff54edb13e956
edarzins <edarzins@ieee.org>
2017-01-16 17:59:26 -0700

    Basic structure with PixyCam integration

--- SteamWorks2017/src/Robot.cpp
+++ SteamWorks2017/src/Robot.cpp
@@ -1,27 +1,75 @@
-#include "WPILib.h"
+#include <iostream>
 #include <iomanip>
+#include "WPILib.h"
+#include "PixyTracker.hpp"
+#include "AHRS.h"
 
-class Robot: public IterativeRobot
-{
+class Robot: public IterativeRobot {
 private:
-	RobotDrive *drive;
-	TalonSRX *lFrontMotor;
-	TalonSRX *lBackMotor;
-	TalonSRX *rFrontMotor;
-	TalonSRX *rBackMotor;
-	Joystick *stick;
-	ADXRS450_Gyro *gyro;
+	enum TrackingMode {
+		kNoTracking = 0,
+		kGearTracking,
+		kBoilerTracking
+	} trackingMode = kNoTracking;
 
-	float deadBand(float stickValue)
-	{
-		if(stickValue <= .2 && stickValue >= -.2)
-		{
+	const int kTrackingBrightness = 20;
+	const int kNormalBrightness = 80;
+
+	PixyTracker *m_pixy;
+	int m_signature = 1;
+	PixyTracker::Target m_target;
+
+	RobotDrive *drive;
+	TalonSRX   *lFrontMotor;
+	TalonSRX   *lBackMotor;
+	TalonSRX   *rFrontMotor;
+	TalonSRX   *rBackMotor;
+	Joystick   *stick;
+	AHRS       *ahrs; // navX MXP
+
+	float slider = 1.0;
+
+	// Deadband filter
+	float deadBand(float stickValue) {
+		if(stickValue <= .15 && stickValue >= -.15) {
 			return 0.0;
-		}
-		else
-		{
+		} else {
 			return stickValue;
 		}
+	}
+
+	// Configure for tracking
+	void startTracking() {
+		// Reset brightness
+
+	}
+
+	// Unconfigure tracking
+	void stopTracking() {
+		// Reset to a fixed pan/tilt position
+		// Reset brightness
+
+	}
+
+	// Process joystick input from driver
+	void doDriverControl() {
+		slider = (stick->GetRawAxis(4)+1)/2;
+
+		drive->MecanumDrive_Cartesian(
+				slider * deadBand(stick->GetRawAxis(0)),
+				slider * deadBand(stick->GetRawAxis(1)),
+				slider * (-1)*deadBand(stick->GetRawAxis(2)),
+				ahrs->GetAngle());
+	}
+
+	// Drive while tracking the gear targets, place the gear, then back up
+	void doGearTracking() {
+
+	}
+
+	//  Drive while tracking the boiler targets to the correct distance, launch fuel
+	void doBoilerTracking() {
+
 	}
 
 	void RobotInit()
@@ -35,39 +83,62 @@ private:
 		rFrontMotor->SetInverted(true);
 		rBackMotor->SetInverted(true);
 
-		gyro = new ADXRS450_Gyro();
-		gyro->Calibrate();
+		try {
+			ahrs = new AHRS(SPI::Port::kMXP);
+		} catch (std::exception& ex ) {
+			std::string err_string = "Error instantiating navX MXP:  ";
+			err_string += ex.what();
+			DriverStation::ReportError(err_string.c_str());
+		}
 
 		drive = new RobotDrive(lFrontMotor, lBackMotor, rFrontMotor, rBackMotor);
-	}
-	void AutonomousInit()
-	{
 
-	}
-	void AutonomousPeriodic()
-	{
-
-	}
-	void TeleopInit()
-	{
-
+		// Create the Pixy instance and start streaming the frames
+		m_pixy = new PixyTracker();
+		m_pixy->startVideo();
 	}
 
-	void TeleopPeriodic()
-	{ // 0: leftX, 1: leftY, 2: left trigger, 3: right trigger, 4: rightX, 5: rightY
-		//
-		//drive->HolonomicDrive(-deadBand(stick->GetRawAxis(4)), -deadBand(stick->GetRawAxis(5)), -deadBand(stick->GetRawAxis(1)));
-		//drive->HolonomicDrive(magnitude, direction, speed);
-		drive->MecanumDrive_Cartesian(stick->GetRawAxis(0), stick->GetRawAxis(1), deadBand(stick->GetRawAxis(4)), gyro->GetAngle());
+	void AutonomousInit() {
+		// Initialize to zero
+		ahrs->ZeroYaw();
+	}
 
+	void AutonomousPeriodic() {
+		switch (trackingMode) {
+		case kNoTracking:
+			break;
+		case kGearTracking:
+			break;
+		case kBoilerTracking:
+			break;
+		}
+	}
+
+	void TeleopInit() {
+		trackingMode = kNoTracking;
+	}
+
+	void TeleopPeriodic() { // 0: leftX, 1: leftY, 2: left trigger, 3: right trigger, 4: rightX, 5: rightY
+		switch (trackingMode) {
+		case kNoTracking:
+			doDriverControl();
+			break;
+		case kGearTracking:
+			doGearTracking();
+			break;
+		case kBoilerTracking:
+			doBoilerTracking();
+			break;
+		}
+
+		// DEBUG
 		std::cout << std::setprecision(3) << std::fixed;
 		std::cout << "Axis 0: " << stick->GetRawAxis(0) << "  Axis 1: " << stick->GetRawAxis(1) << "  Axis 4: " << stick->GetRawAxis(4) << std::endl;
 
 		std::cout << "Motors = LF: " << lFrontMotor->Get() << "  RF: " << rFrontMotor->Get() << "  LR: " <<
-				                    lBackMotor->Get() << "  RR: " << rBackMotor->Get() << std::endl;
+				lBackMotor->Get() << "  RR: " << rBackMotor->Get() << std::endl;
 
-		std::cout << "Angle: " << gyro->GetAngle() << "  Rate: " << gyro->GetRate() << std::endl;
-
+		std::cout << "Angle: " << ahrs->GetAngle() << "  Rate: " << ahrs->GetRate() << std::endl;
 	}
 };
 
